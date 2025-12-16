98a2fd120cd86e6a2764c5c65edf9f5afe821284
foodpenguin <ark009770@gmail.com>
2025-12-16 14:44:01 +0800

    更新

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../App.vue') // Redirect to App or just silence the warning
+    }
+  ],
 })
 
 export default router
